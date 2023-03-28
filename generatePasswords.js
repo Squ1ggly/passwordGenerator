e2bc810440c9ba9350890ca8a23f52298813bb09
Squ1ggly <squ1gglyzerocool@gmail.com>
2023-03-28 13:21:40 +1000

    FIX: Added package for user prompting

--- generatePasswords.js
+++ generatePasswords.js
@@ -1,30 +1,52 @@
 const chars = require("./dictionary.json"); // Word dictionary to make passwords from
-const { writeFileSync } = require("fs");
+const {question} = require('readline-sync');
+
 
 /**
  *
  * @param {string[]} Dictionary
- * @param {number} NumberOfPasswords
- * @param {number} passwordLength
- * @param {string} separator
- * @param {boolean} TitleCase
- * @returns {string[]} Returns the passwords string array
  */
-function generatePasswords(Dictionary, NumberOfPasswords, passwordLength, separator = "-", TitleCase = false) {
-  if (NumberOfPasswords > 100000) return new Error("Cannot have number over 100k");
-  if (passwordLength > 100) return new Error("Cannot have have password over 100 words");
-  if (passwordLength < 2) return new Error("Password length must be at least 2");
-  let returnValue = [];
+function generatePassphrases(Dictionary) {
+  let NumberOfPasswords = 3;
+  let passwordLength = 3;
+  let separator = "-";
+  let TitleCase = false;
+
+  let defaults = question("Would you like to use all default values? (yes or no), Enter for yes: ");
+  while (defaults !== "yes" && defaults !== "no") {
+    console.log("Enter either yes or no");
+    defaults = question("Would you like to use all default values? (yes or no), Enter for yes: ");
+  }
+
+  if (defaults.toLowerCase() === "no") {
+    NumberOfPasswords = question("Number of passwords you want to generate, Enter for default (3): ") || 3;
+    while (typeof parseInt(NumberOfPasswords) != "number" || isNaN(parseInt(NumberOfPasswords)) || parseInt(NumberOfPasswords) > 100000) {
+      console.log("Invalid Values");
+      NumberOfPasswords = question("Number of passwords you want to generate, Enter for default (3): ") || 1;
+    }
+
+    passwordLength = question("Number of words you want in your passphrase, Enter for default (3): ") || 3;
+    while (typeof parseInt(passwordLength) != "number" || isNaN(parseInt(passwordLength)) || parseInt(passwordLength) > 100) {
+      console.log("Invalid Values");
+      passwordLength = question("Number of words you want in your passphrase, Enter for default (3): ") || 3;
+    }
+
+    separator = question("The separator, Enter for default (-): ") || "-";
+    TitleCase = question("Title case? Enter for default (false): ") || false;
+    if (typeof TitleCase !== "boolean") TitleCase = false;
+  }
+
+  let returnCsv = [];
   for (let i = 0; i < NumberOfPasswords; i++) {
     let password = "";
     for (let j = 1; j <= passwordLength; j++) {
-      let word = Dictionary[Math.floor(Math.random() * Dictionary.length)] + (j === passwordLength ? "" : separator);
+      let word = Dictionary[Math.floor(Math.random() * Dictionary.length)] + (j >= passwordLength ? "" : separator);
       if (TitleCase) word = word.charAt(0).toUpperCase() + word.slice(1);
       password += word;
     }
-    returnValue.push(password);
+    returnCsv.push(password);
   }
-  return returnValue;
+  return returnCsv;
 }
 
-writeFileSync(__dirname + "/Passwords.json", JSON.stringify(generatePasswords(chars, 5, 3, ".", true), null, 2));
+console.log("\n\n" + generatePassphrases(chars).join(",\n") + "\n\n");
